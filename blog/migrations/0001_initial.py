49a46e766b0c100f66796d1a1c232e88248ff1a2
Ivan Suroegin <ivan.suroegin@gmail.com>
2016-12-23 18:55:27 +0300

    First commit, add files to GitHub

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=datetime.datetime(2016, 12, 23, 15, 0, 31, 759480, tzinfo=utc))),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
---
+++ blog/migrations/0002_auto_20161223_1801.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='created_date',
+            field=models.DateTimeField(default=datetime.datetime(2016, 12, 23, 15, 1, 20, 217044, tzinfo=utc)),
+        ),
+    ]
